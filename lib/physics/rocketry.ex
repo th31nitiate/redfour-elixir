a84ca1df31a4ae9c6aaf309f03c4d5a2a7c59532
m3rl1n th31nitiate <m3rl1n@devlen.co.uk>
2017-12-27 19:58:36 +0000

    Refactoring the converter

--- lib/physics/rocketry.ex
+++ lib/physics/rocketry.ex
@@ -7,15 +7,29 @@ defmodule Converter do
     velocity / 1000
   end
 
+  def to_meters do
+    val * 1000      
+  end
+
   def to_light_seconds({:miles, miles} = val, precision: precision) do
-    (miles * 5.36819e-6) |> round_up
+    (miles * 5.36819e-6) |> round_to(precision)
   end
 
   def to_light_seconds({:meters, meters} = val, precision: precision)  do
-    (meters * 3.335638620368e-9) |> round_up
+    (meters * 3.335638620368e-9) |> round_to(precision)
   end
 
-  def round_up(val) when is_float(val), do: trunc(val)
+  def to_light_seconds({:feet, feet} = val, precision: precision) do
+    (feet * 1.016702651488166404e-9) |> round_to(precision)
+  end
+
+  def to_light_seconds({:inches, inches} = val, precision: precision)  do
+    (inches * 8.472522095734715723e-11) |> round_to(precision)
+  end
+
+  def round_up(val, precision) when is_float(val) do
+      Float.round(val, precision)
+  end
 end
 
 defmodule Physics.Rocketry do
