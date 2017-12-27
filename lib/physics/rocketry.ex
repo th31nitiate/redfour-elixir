ad07fc867dc616775bd19ec1701278fa77115a2b
m3rl1n th31nitiate <m3rl1n@devlen.co.uk>
2017-12-27 20:03:44 +0000

    mofidied the converter module using the case statement and pattern matching, also radically refactered it.

--- lib/physics/rocketry.ex
+++ lib/physics/rocketry.ex
@@ -32,6 +32,37 @@ defmodule Converter do
   end
 end
 
+defmodule ConverterTwo do
+
+  def to_nearest_tenth(val) do
+        Float.round(val, 1)
+  end
+    
+  def to_km(velocity) do
+        velocity / 1000
+  end
+    
+  def to_meters do
+        val * 1000      
+  end
+    
+  
+   def to_light_seconds({unit, val}, precision: precision) do
+     case unit do
+       :miles -> from_miles(val)
+       :meters -> from_meters(val)
+       :feet -> from_feet(val)
+       :inches -> from_inches(val)
+     end |> round_to(precision) 
+   end
+  
+   defp from_miles(val), do: val * 5.36819e-6
+   defp from_meters(val), do: val * 3.335638620368e-9
+   defp from_feet(val), do: val * 1.016702651488166404e-9
+   defp from_inches(val), do: val * 8.472522095734715723e-11
+   defp round_to(val, precision), do: Float.round(val, precision)
+end
+
 defmodule Physics.Rocketry do
 
   def escape_velocity(:earth) do
