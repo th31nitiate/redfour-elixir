d688c9d96d565d72e4b6a09ecf226510cb22e795
m3rl1n th31nitiate <m3rl1n@devlen.co.uk>
2017-12-28 14:28:30 +0000

    Added a few new modules by sepration and also refactored code to look cleaner by removing pre module names when calling a funcation.

--- lib/physics/rocketry.ex
+++ lib/physics/rocketry.ex
@@ -1,29 +1,25 @@
 defmodule Physics.Rocketry do
   import Converter
   import Calcs
-
-  @moon %{mass: 7.35e22, radius: 1.738e6}
-  @mars %{mass: 6.39e23, radius: 3.4e6}
-  @earth %{mass: 5.972e24, radius: 6.371e6}
-  @newtons_constant 6.67e-11
-
+  import Physics.Laws
+  import Planets
 
   # shortend the code a bit, by doing one liners for diffault values
-  def escape_velocity(:earth), do: @earth |> escape_velocity
-  def escape_velocity(:moon), do: @moon |> escape_velocity
-  def escape_velocity(:mars), do: @mars |> escape_velocity
+  def escape_velocity(:earth), do: earth |> escape_velocity
+  def escape_velocity(:moon), do: moon |> escape_velocity
+  def escape_velocity(:mars), do: mars |> escape_velocity
 
 
   def escape_velocity(planet) when is_map(planet) do
     planet
       |> calculate_escape
-      |> Converter.to_km
-      |> Converter.to_nearest_tenth
+      |> to_km
+      |> to_nearest_tenth
   end
 
   defp calculate_escape(%{mass: mass, radius: radius}) do
-    2 * @newtons_constant * mass / radius
-      |> Calcs.square_root
+    2 * newtons_gravitational_constant * mass / radius
+      |> square_root
   end
 
 end
