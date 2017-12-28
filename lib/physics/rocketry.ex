5543e502c6eec359c759649d0a086c9cc447d690
m3rl1n th31nitiate <m3rl1n@devlen.co.uk>
2017-12-28 14:22:47 +0000

    Refactored the Ev modules

--- lib/physics/rocketry.ex
+++ lib/physics/rocketry.ex
@@ -7,20 +7,11 @@ defmodule Physics.Rocketry do
   @earth %{mass: 5.972e24, radius: 6.371e6}
   @newtons_constant 6.67e-11
 
-  def escape_velocity(:earth) do
-    @earth
-      |> escape_velocity
-  end
 
-  def escape_velocity(:moon) do
-    @moon
-      |> escape_velocity
-  end
-
-  def escape_velocity(:mars) do
-    @mars
-      |> escape_velocity
-  end
+  # shortend the code a bit, by doing one liners for diffault values
+  def escape_velocity(:earth), do: @earth |> escape_velocity
+  def escape_velocity(:moon), do: @moon |> escape_velocity
+  def escape_velocity(:mars), do: @mars |> escape_velocity
 
 
   def escape_velocity(planet) when is_map(planet) do
@@ -29,15 +20,6 @@ defmodule Physics.Rocketry do
       |> Converter.to_km
       |> Converter.to_nearest_tenth
   end
-
-
-  def escape_velocity_two(planet) when is_map(planet) do
-    planet
-      |> calculate_escape
-      |> ConverterTwo.to_km
-      |> ConverterTwo.to_nearest_tenth
-  end
-
 
   defp calculate_escape(%{mass: mass, radius: radius}) do
     2 * @newtons_constant * mass / radius
