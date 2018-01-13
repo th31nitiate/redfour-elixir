27db0c4a56233816ae7d168ecf23bb90d80995e7
m3rl1n th31nitiate <m3rl1n@devlen.co.uk>
2018-01-13 20:01:16 +0000

    This is generally before moving on to the list + recursion section.

---
+++ programing_elixir_exercises/commands.ex
@@ -0,0 +1,9 @@
+:io.format("~.2f~n", [2.0/3.0]) # This one i did not understand fully mainly the starting bit, these erlang definitiona ~.2f~n
+
+System.get_env("PWD") # This one I get
+
+Path.extname("commads.ex")
+
+System.cwd
+
+System.cmd "uname", ["-a"]
--- programing_elixir_exercises/summodule.ex
+++ programing_elixir_exercises/summodule.ex
@@ -17,4 +17,30 @@ defmodule Calcint do
     #
     def gcd(x, 0), do: x
     def gcd(x, y), do: gcd(y, rem(x, y))
+
+
+
+    # module and funcations 6
+    #Again this one was a bit beyound me. I wasnt too sure as to the ojective. 
+    #I am not sure what the _ infront means. maybe it denoting private methods.
+    #I think what is confusing here is the gussing process.
+
+
+    def guess(actual, range = low..high) do 
+        guess = div(low+high, 2)
+        IO.puts "Is it #{guess}?"
+        _guess(actual, guess, range)
+    end
+    
+    defp _guess(actual, actual, _), 
+        do: IO.puts "Yes, it's #{actual}"
+    
+    defp _guess(actual, guess,  _low..high)
+        when guess < actual,
+        do: guess(actual, guess+1..high) #the secret is happing here i guess
+    
+    defp _guess(actual, guess,  low.._high) 
+        when guess > actual, 
+        do: guess(actual, low..guess-1) #This is some type of recursion that basicly checks range based on the guard clause value
+        #This will basically check all values present based on less or nmore than acuatal value.
 end
